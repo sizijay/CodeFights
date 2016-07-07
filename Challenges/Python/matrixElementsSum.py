907e42460ce13a71ecf7daf235496f12d35b29a1
agusmakmun <summon.agus@gmail.com>
2016-07-07 21:23:47 +0700

    Added matrixElementsSum

---
+++ Challenges/Python/matrixElementsSum.py
@@ -0,0 +1,11 @@
+# Question: https://github.com/Kafiil/CodeFights/blob/master/tasks/matrixElementsSum.md
+
+def matrixElementsSum(matrix):
+    result = 0
+    for i in range(len(matrix[0])):
+        for j in range(len(matrix)):
+            if matrix[j][i] == 0:
+                break
+            result += matrix[j][i]
+    return result
+
